0215afcd508ec15762683cca4ef0e7305ca89a65
Bruno Henrique <67505790+spiderbuddy411@users.noreply.github.com>
2022-11-27 18:45:05 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require ('sequelize');
 
-const conn = new Sequelize('brdata', 'sga', 'brd@t@', {
+const conn = new Sequelize('brdata', 'sga', '123', {
     host: '127.0.0.1',
     dialect: 'mysql'
 });
